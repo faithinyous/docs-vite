8c922b007624fdfe5a65a1c01941b8fc412cd1e2
Apiporn Simapornchai <faithinyouuu@gmail.com>
2023-06-03 23:42:22 +0700

    add google tag

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -2,6 +2,20 @@ import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  head: [
+    [
+      "script",
+      {
+        async: true,
+        src: "https://www.googletagmanager.com/gtag/js?id=G-FBBDK66XKD",
+      },
+    ],
+    [
+      "script",
+      {},
+      "window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', 'G-FBBDK66XKD');",
+    ],
+  ],
   title: "Faith Docs",
   description: "Docs created by faith",
   // base: "/docs/",
